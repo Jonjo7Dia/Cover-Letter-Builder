c937f97051b2d4ac32ff016ab2f79a27dccd501b
Jonjo7Dia <jonosphgames@gmail.com>
2023-07-10 22:02:22 +0200

    Add logos

--- components/seo.tsx
+++ components/seo.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import logo from "assets/tApplicationLogo.svg";
 const SEO: React.FC = () => {
   return (
     <Head>
@@ -24,7 +23,7 @@ const SEO: React.FC = () => {
           "TailoredApplication is your one-stop solution for crafting personalized, effective cover letters. Our advanced web application uses the information from your CV to generate custom cover letters tailored to any job application. Stand out from the crowd, captivate employers, and land your dream job with TailoredApplication. Your journey towards a successful job application starts here!"
         }
       />
-      <meta key="og:image" property="og:image" content={logo} />
+      <meta key="og:image" property="og:image" content={"assets/logoPng.png"} />
       <meta key="og:url" property="og:url" content={"/"} />
       <link rel="icon" href="/favicon.ico" />
     </Head>
