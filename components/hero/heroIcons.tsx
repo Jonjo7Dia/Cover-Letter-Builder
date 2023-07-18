5e1f8d1aaeca5546e9ba0271d945e14a73446459
Jonjo7Dia <jonosphgames@gmail.com>
2023-07-18 19:51:35 +0200

    Add basic redesign to hero

---
+++ components/hero/heroIcons.tsx
@@ -0,0 +1,52 @@
+import styles from "styles/component/hero.module.scss";
+import money from "assets/icons/Money.svg";
+import clock from "assets/icons/Clock.svg";
+import key from "assets/icons/Key.svg";
+import person from "assets/icons/Person.svg";
+import Image from "next/image";
+import { useState } from "react";
+
+type iconLogo = {
+  source: object;
+  title: string;
+};
+
+const HeroIcons = () => {
+  const [hoveredIcon, setHoveredIcon] = useState<number | null>(null);
+  const icons: iconLogo[] = [
+    { source: money, title: "Save Money" },
+    { source: clock, title: "Save Time" },
+    { source: key, title: "Target Keywords" },
+    { source: person, title: "Personally Generated" },
+  ];
+  return (
+    <div className={styles["hero__icons"]}>
+      {icons.map((icon: any, index: number) => {
+        return (
+          <div className={styles["hero__icon-wrapper"]} key={index}>
+            <Image
+              src={icon.source}
+              alt={`${icon.title} icon`}
+              className={styles["hero__icon"]}
+              onMouseOver={() => {
+                setHoveredIcon(index);
+              }}
+              onMouseOut={() => {
+                setHoveredIcon(null);
+              }}
+            />
+            <div
+              className={`${styles["hero__icon-info"]} ${
+                hoveredIcon === index ? styles["hero__icon-info--visible"] : ""
+              }`}
+            >
+              {icon.title}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default HeroIcons;
