c7400fa9da22960aa9a2fab629352d41d5dace82
Jonjo7Dia <jonosphgames@gmail.com>
2023-07-19 22:53:20 +0200

    Update SEO

--- components/seo.tsx
+++ components/seo.tsx
@@ -3,6 +3,11 @@ const SEO: React.FC = () => {
   return (
     <Head>
       <title key="title">Tailored Applications</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="canonical" href="https://tailoredapplication.com" />
+
       <meta
         key="description"
         name="description"
@@ -10,6 +15,7 @@ const SEO: React.FC = () => {
           "TailoredApplication is your one-stop solution for crafting personalized, effective cover letters. Our advanced web application uses the information from your CV to generate custom cover letters tailored to any job application. Stand out from the crowd, captivate employers, and land your dream job with TailoredApplication. Your journey towards a successful job application starts here!"
         }
       />
+
       <meta key="og:type" property="og:type" content="website" />
       <meta
         key="og:title"
@@ -26,10 +32,13 @@ const SEO: React.FC = () => {
       <meta
         key="og:image"
         property="og:image"
-        content={"../assets/logoPng.png"}
+        content={"https://tailoredapplication.com/assets/logoPng.png"}
       />
-      <meta key="og:url" property="og:url" content={"/"} />
-      <link rel="icon" href="/favicon.ico" />
+      <meta
+        key="og:url"
+        property="og:url"
+        content={"https://tailoredapplication.com"}
+      />
     </Head>
   );
 };
