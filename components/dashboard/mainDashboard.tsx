a98e20917d1f6d9ff90d277a48ec1e9f6145bdbf
Jonjo7Dia <jonosphgames@gmail.com>
2023-08-12 15:42:25 +0300

    Reset

--- components/dashboard/mainDashboard.tsx
+++ components/dashboard/mainDashboard.tsx
@@ -1,30 +1,10 @@
 import AddJobStep from "components/addJobStep";
 import GeneralProduct from "components/generalProduct";
 import { useAuth } from "contexts/authContext";
-import { useUser } from "contexts/userContext";
-import { usePdfParse } from "hooks/pdfHooks";
-import { useEffect } from "react";
-
 import styles from "styles/dashboard/mainDashboard.module.scss";
 
 const MainDashboard = () => {
   const { user } = useAuth();
-  const { error, parsePdf } = usePdfParse();
-  const { parsedPdfText } = useUser();
-  useEffect(() => {
-    const parse = async () => {
-      if (user.pdfURL) {
-        const isParsed = await parsePdf(user.pdfURL);
-        if (isParsed) {
-          console.log("hello");
-          console.log(parsedPdfText);
-        } else {
-          console.log(error);
-        }
-      }
-    };
-    parse();
-  }, []);
 
   return (
     <div className={styles["main-dashboard"]}>
