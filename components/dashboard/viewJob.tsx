ee8d34f4633d013812982ec93ae2c38449c9dce4
Jonjo7Dia <jonosphgames@gmail.com>
2023-08-25 13:00:10 +0200

    Fix job ad view

--- components/dashboard/viewJob.tsx
+++ components/dashboard/viewJob.tsx
@@ -32,7 +32,7 @@ const ViewJob: React.FC<viewJob> = ({
 
     return day + "/" + month + "/" + year;
   }
-  console.log(company, position, date, replied, interview, offer, ad);
+  console.log(ad);
   return (
     <div className={styles["viewJob"]}>
       <h4 className={styles["viewJob__title"]}> Company Name: {company}</h4>
